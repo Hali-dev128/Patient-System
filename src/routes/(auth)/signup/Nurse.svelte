1c8b41796aa12d9384322292c20ec9d9e30d0cb0
Hali-dev128 <hali.dev128@gmail.com>
2025-10-19 15:25:35 +0100

    Update Nurse.svelte

--- src/routes/(auth)/signup/Nurse.svelte
+++ src/routes/(auth)/signup/Nurse.svelte
@@ -1,2 +1,3 @@
-<input type="text" name="reg" id="reg" required placeholder="Enter your registration number...">
+<input type="text" name="reg" id="reg" required placeholder="Enter your NMCN license number...">
 <input type="text" name="unit" id="unit" required placeholder="Enter your department/unit...">
+<input type="text" name="url" id="url" required placeholder="Enter your license_document_url...">
