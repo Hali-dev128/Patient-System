db7a6e55434b0689d1d8d4a5bfa4428a81bd7ff8
Hali-dev128 <hali.dev128@gmail.com>
2025-10-19 15:25:13 +0100

    Update Doctor.svelte

--- src/routes/(auth)/signup/Doctor.svelte
+++ src/routes/(auth)/signup/Doctor.svelte
@@ -1,2 +1,3 @@
-<input type="text" name="license" id="license" required placeholder="Enter your license Number...">
+<input type="text" name="license" id="license" required placeholder="Enter your Folio Number...">
 <input type="text" name="specialization" id="specialization" required placeholder="Enter your specialization...">
+<input type="text" name="url" id="url" required placeholder="Enter your license_document_url...">
