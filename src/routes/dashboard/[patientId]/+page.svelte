40b8fb27df3d672e6fa9984341ffd92e497e4a91
Hali-dev128 <hali.dev128@gmail.com>
2025-10-31 21:44:19 +0100

    Update +page.svelte

--- src/routes/dashboard/[patientId]/+page.svelte
+++ src/routes/dashboard/[patientId]/+page.svelte
@@ -72,7 +72,7 @@
                 <div class="flex justify-between items-center">
                     <h3 class="font-bold text-xl">Patient's Vitals</h3>
                     {#if $Role == 'nurse'}
-                        <Button light={true}>
+                        <Button light={true} on:click={() => variable2 = true}>
                             <svg data-v-14c8c335="" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus lucide-icon customizable"><path d="M5 12h14"></path><path d="M12 5v14"></path></svg>
                         </Button>
                     {/if}
@@ -104,7 +104,7 @@
                 <div class="flex justify-between items-center">
                     <h3 class="font-bold text-xl">Nurse Notes</h3>
                     {#if $Role == 'nurse'}
-                        <Button light={true} on:click={() => variable2 = true}>
+                        <Button light={true}>
                             <svg data-v-14c8c335="" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus lucide-icon customizable"><path d="M5 12h14"></path><path d="M12 5v14"></path></svg>
                         </Button>
                     {/if}
